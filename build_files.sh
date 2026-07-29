ae4fc93761bb5e6c5bc4374edaa086fc248b570d
Arnav Shukla <arnavshukla09@github.com>
2026-07-29 21:04:00 +0530

    Fix Vercel PIP externally managed environment error

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "BUILD START"
-pip3 install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 python3 PahalFoundation/manage.py collectstatic --noinput --clear
 echo "BUILD END"
