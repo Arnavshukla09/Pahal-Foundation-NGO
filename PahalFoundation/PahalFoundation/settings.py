20840be6bcc46218008af7f74b4ffbd438014aff
Arnav Shukla <arnavshukla09@github.com>
2026-07-29 21:08:15 +0530

    Fix Vercel static routing issue by outputting to a static subfolder

--- PahalFoundation/PahalFoundation/settings.py
+++ PahalFoundation/PahalFoundation/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # ============================================================
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # WhiteNoise: serve compressed static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
